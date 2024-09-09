4cdb580a0395ca0a94512baa1bbb1aa2fe6c9a3d
joao pedro santos <jopesame@gmail.com>
2024-09-09 16:08:06 -0300

    feat: update navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, CommonModule, CdkAccordionModule],
+  imports: [RouterLink, MatMenuModule, MatButtonModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
